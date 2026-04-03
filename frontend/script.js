122916de54e3186f204c2e3fd31963c30ca7f470
Nat Valentine <sodiumvii@gmail.com>
2026-04-02 22:49:24 -0300

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+async function shorten() {
+  const url = document.getElementById("urlInput").value;
+
+  const res = await fetch(
+    "https://ceoolgnpv7.execute-api.us-east-1.amazonaws.com/shorten",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    },
+  );
+
+  const data = await res.json();
+
+  const shortUrl = `https://ceoolgnpv7.execute-api.us-east-1.amazonaws.com/${data.shortUrl}`;
+  document.getElementById("result").innerHTML =
+    `Short URL: <a href="${shortUrl}" target="_blank">${shortUrl}</a>`;
+}
